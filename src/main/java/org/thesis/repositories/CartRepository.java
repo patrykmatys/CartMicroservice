9ce09131d0eed0230b91981428c995e84d7ead6d
Patryk Matys <patrykmatys0@gmail.com>
2023-08-22 17:31:06 +0200

    remove unused imports

--- src/main/java/org/thesis/repositories/CartRepository.java
+++ src/main/java/org/thesis/repositories/CartRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.thesis.models.Cart;
 
-import java.util.List;
-
 @Repository
 public interface CartRepository extends MongoRepository<Cart, String> {
     Cart findCartByUser(String user);
